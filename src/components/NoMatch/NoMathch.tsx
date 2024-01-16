90c84d2c58ddb375e5e49b7c4ff52656e3bba092
anivash <andr.ivas12@gmail.com>
2024-01-16 11:47:23 +0100

    update noPage styles

--- src/components/NoMatch/NoMathch.tsx
+++ src/components/NoMatch/NoMathch.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Screen from '../../containers/Screen';
 
+import './NoMatch.scss';
+
 export const NoMatch = () => {
     return (
         <Screen>
-            <div data-testid='NoMatch'>
+            <div className='noMatch' data-testid='NoMatch'>
                 <h2>Wrong URL</h2>
                 <p>
                     <Link to='/'>Go to the home page</Link>
